ddd3117557aafebd30bd7d6aee7db06c41b2d31c
Jord <dorohenkojord@gmail.com>
2020-11-23 15:26:42 +0200

    15:26

--- src/module.js
+++ src/module.js
@@ -1,1 +1,6 @@
 console.log('Module.js');
+
+async function start (){
+    await Promise.resolve()
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,17 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
+    
   }
 };
